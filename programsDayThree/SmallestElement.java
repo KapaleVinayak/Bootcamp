f231bf14f503550a75d64b473e7a1e9e2b3980e8
KapaleVinayak <vinayakkapale18@gmail.com>
2024-01-24 17:31:55 +0530

    small

---
+++ programsDayThree/SmallestElement.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class SmallestElement{
+    public static void main(String[] args) {
+        int numbers[] = new int[] {12,3,4,5,7,8};
+        Arrays.sort(numbers);
+        System.out.println("Smallest Element int the arrray is : " + numbers[0]);
+
+    }
+}
