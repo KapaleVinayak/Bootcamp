83636f688930386c38f20d88cc28a95a699ed5f1
KapaleVinayak <vinayakkapale18@gmail.com>
2024-01-25 18:57:58 +0530

    new

--- programsDayThree/SecondLargest.java
+++ programsDayThree/SecondLargest.java
@@ -1,19 +1,20 @@
 class SecondLargest{
-    public static void main(String[] args) {
-        int numbers[] = {10,24,36,2,30,22};
-        int temp;
-        for(int i = 0 ;i<numbers.length-1;i++)
-        {
-            for(int j = i+1;j<numbers.length;j++)
-            {
-                if(numbers[i]<numbers[j])
-                {
-                  temp = numbers[i];
-                  numbers[i]= numbers[j];
-                  numbers[j] = numbers[i];
-                }
-            }
-            System.out.println("Second Largest "+ numbers[1]);
+    public static void main(String args[]){
+        int temp, size;
+        int array[] = {10, 20, 25, 63, 43, 100};
+        size = array.length;
+  
+        for(int i = 0; i<size; i++ ){
+           for(int j = i+1; j<size; j++){
+  
+              if(array[i]>array[j]){
+                 temp = array[i];
+                 array[i] = array[j];
+                 array[j] = temp;
+              }
+           }
         }
-    }
+        System.out.println("The second largest number is:: "+array[size-2]);
+     }
+  
 }
